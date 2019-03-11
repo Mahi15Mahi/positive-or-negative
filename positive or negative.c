99add94b70601c0b0c8696532f946f227f216b00
Mahi15Mahi <48052950+Mahi15Mahi@users.noreply.github.com>
2019-03-11 16:11:28 +0530

    Update positive or negative.c

--- positive or negative.c
+++ positive or negative.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-    double number;
-
-    printf("Enter a number: ");
-    scanf("%lf", &number);
-
-    if (number <= 0.0)
-    {
-        if (number == 0.0)
-            printf("You entered 0.");
-        else
-            printf("You entered a negative number.");
-    }
+    int num;
+     printf("Enter a number: ");
+    scanf("%d", &num);
+        if (num > 0)
+        printf("%d is a positive number \n",num);
+    else if (num < 0)
+        printf("%d is a negative number \n",num);
     else
-        printf("You entered a positive number.");
-    return 0;
-}
+        printf("0 is neither positive or negative");
+
+   
+
+
+    
