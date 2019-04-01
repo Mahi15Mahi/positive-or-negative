8bd7a11a61a7aaa460e776f67f45032aa76dc0e4
Mahi15Mahi <48052950+Mahi15Mahi@users.noreply.github.com>
2019-04-01 16:00:03 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+  char c;
+  int islowercasevowel, isuppercasevowel;
+  scanf("%c",&c);
+  islowercasevowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+  isuppercasevowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+  if(islowercasevowel||isuppercasevowel)
+    printf("%c is a vowel.",c);
+  else
+    printf("%c is a constant.",c);
+  return 0;
